ec67f881abe468907ce755889d04b30eed1be8c3
lenzap497 <lenzap497@gmail.com>
2018-12-27 15:25:49 -0800

    fixed table_of_contents.rb

--- week1/ch06/table_of_contents.rb
+++ week1/ch06/table_of_contents.rb
@@ -5,6 +5,12 @@
 line_width = 50
 puts('Table of Contents'.center(line_width))
 puts''
-puts('Chapter 1:  Getting Started'.ljust(line_width/2) + 'page   1'.rjust(line_width/2))
-puts('Chapter 2:  Numbers        '.ljust(line_width/2) + 'page   9'.rjust(line_width/2))
-puts('Chapter 3:  Letters        '.ljust(line_width/2) + 'page  13'.rjust(line_width/2))
+puts('Chapter 1:  Getting Started         '.ljust(line_width/2) + 'page   1'.rjust(line_width/2))
+puts('Chapter 2:  Numbers                 '.ljust(line_width/2) + 'page   9'.rjust(line_width/2))
+puts('Chapter 3:  Letters                 '.ljust(line_width/2) + 'page  13'.rjust(line_width/2))
+puts('Chapter 4:  Variables and Assignment'.ljust(line_width/2) + 'page  17'.rjust(line_width/2))
+puts('Chapter 5:  Mixing it Up            '.ljust(line_width/2) + 'page  21'.rjust(line_width/2))
+puts('Chapter 6:  More About Methods      '.ljust(line_width/2) + 'page  27'.rjust(line_width/2))
+puts('Chapter 7:  Flow Control            '.ljust(line_width/2) + 'page  37'.rjust(line_width/2))
+puts('Chapter 8:  Arrays and Iterators    '.ljust(line_width/2) + 'page  51'.rjust(line_width/2))
+puts('Chapter 9:  Writing Your Own Methods'.ljust(line_width/2) + 'page  57'.rjust(line_width/2))
