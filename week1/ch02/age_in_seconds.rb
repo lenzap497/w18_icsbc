129d840d3b67683d7a7dbf0c02ad34d74ff9f6f8
lenzap497 <lenzap497@gmail.com>
2018-12-27 11:16:13 -0800

    completed age_in_seconds.rb and minutes_in_decade.rb

--- week1/ch02/age_in_seconds.rb
+++ week1/ch02/age_in_seconds.rb
@@ -5,3 +5,16 @@
 
 
 ### Your Code Here ###
+
+years = 2018 - 1868
+leap_years = 36
+hours_in_day = 24
+minutes_in_hour = 60
+seconds_in_minute = 60
+
+days_older = (365 - 3) - (31 + 28 + 23)
+days_in_years = years  * 365 + leap_years
+days_tot = days_older + days_in_years
+seconds = days_tot * hours_in_day * minutes_in_hour *seconds_in_minute
+
+puts seconds
--- week1/ch02/minutes_in_decade.rb
+++ week1/ch02/minutes_in_decade.rb
@@ -3,3 +3,17 @@
 
 
 ### Your Code Here ###
+
+years_in_decade = 10
+days_in_year = 365
+leap_years = 3
+hours_in_day = 24
+minutes_in_hour = 60
+
+days_in_decade = days_in_year * years_in_decade + 1*leap_years
+
+hours_in_decade = hours_in_day * days_in_decade
+
+minutes_in_decade = minutes_in_hour * hours_in_decade
+
+puts minutes_in_decade
