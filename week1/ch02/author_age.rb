77bc884efdc1e4d81b34cc9e022aaf0ea8a4e5c2
lenzap497 <lenzap497@gmail.com>
2018-12-27 11:30:22 -0800

    author_age.rb done

--- week1/ch02/author_age.rb
+++ week1/ch02/author_age.rb
@@ -5,3 +5,10 @@
 
 
 ### Your Code Here ###
+years = 2018.0 - 1999.0
+days_in_years = 365.25 * years
+days_other = 29.0 + 30.0 + 31.0 - 3.0 # days that aren't included in the calculation in line 9
+days_tot = days_in_years + days_other
+
+author_age = days_tot / 365.25
+puts author_age
