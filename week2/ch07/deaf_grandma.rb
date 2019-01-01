02a46999179e5d9af52c610a47edb0e119b5964d
lenzap497 <lenzap497@gmail.com>
2018-12-31 19:20:21 -0800

    deaf_grandma.rb done

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,7 +1,14 @@
 puts 'COME GIVE GRANNY A KISS!'
 
-while () # Fill in the while condition
-  ### Your Code Here ###
+while true
+  speech = gets.chomp
+  if speech == 'BYE'
+    break
+  elsif speech != speech.upcase
+    puts 'HUH?! SPEAK UP, SONNY!'
+  elsif speech == speech.upcase
+    puts 'NO, NOT SINCE ' + (1930 + rand(21)).to_s + '!'
+  end
 end
 
 puts 'OK SONNY, TALK AGAIN SOON'
