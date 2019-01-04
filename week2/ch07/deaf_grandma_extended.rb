e3fe0289d9e034a039d439742d46fc54b9ebf8bb
lenzap497 <lenzap497@gmail.com>
2019-01-03 19:37:37 -0800

    deaf_grandma_extended.rb done

--- week2/ch07/deaf_grandma_extended.rb
+++ week2/ch07/deaf_grandma_extended.rb
@@ -1,8 +1,23 @@
 puts 'COME GIVE GRANNY A KISS!'
 
 bye_counter = 0
-while () # Fill in the while condition
-  ### Your Code Here ###
+
+while true
+  speech = gets.chomp
+  if speech != speech.upcase
+    puts 'HUH?! SPEAK UP, SONNY!'
+    bye_counter = 0
+  elsif speech == speech.upcase
+    if speech != 'BYE'
+      bye_counter = 0
+    elsif speech == 'BYE'
+      bye_counter = bye_counter + 1
+      if bye_counter == 3
+        break
+      end
+    end
+    puts 'NO, NOT SINCE ' + (1930 + rand(21)).to_s + '!'
+  end
 end
 
 puts 'OK SONNY, TALK AGAIN SOON'
