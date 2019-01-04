36eace1f3f9b3a448fa50851ce2a9516dc7c4807
lenzap497 <lenzap497@gmail.com>
2019-01-04 13:37:41 -0800

    array_sort.rb done

--- week2/ch08/array_sort.rb
+++ week2/ch08/array_sort.rb
@@ -3,8 +3,15 @@ word_list = []
 puts 'Please enter the desired words (one per line), and press enter when finished:'
 
 # Fetch the words, then output the sorted list
-while ()
-
+array = []
+while true
+  entry = gets.chomp
+  if entry != ''
+    array.push entry
+  elsif entry == ''
+    break
+  end
+  word_list = array.sort
 end
 
 puts word_list # Make sure its sorted!
